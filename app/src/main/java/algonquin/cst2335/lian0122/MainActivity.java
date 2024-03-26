b2609d2c88810732bb749a3af13d9c45b894e4b3
itsolidude <flexibleoli.12@gmail.com>
2024-03-26 15:57:32 -0400

    Added Main menu basic setup and activity structure for sunset app - O.k

--- app/src/main/java/algonquin/cst2335/lian0122/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/lian0122/MainActivity.java
@@ -2,13 +2,25 @@ package algonquin.cst2335.lian0122;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import algonquin.cst2335.lian0122.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnOpenSunriseSunsetLookup.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, Sun_MainActivity.class);
+            startActivity(intent);
+        });
     }
 }
+
