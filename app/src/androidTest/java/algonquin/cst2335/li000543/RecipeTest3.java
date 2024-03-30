85cfa65f38445f75ff2504cbe589d1f8d94364f5
SeanLee777 <146900956+SeanLee777@users.noreply.github.com>
2024-03-29 23:42:51 -0300

    Revert "Revert "Shanghao li recipe""

---
+++ app/src/androidTest/java/algonquin/cst2335/li000543/RecipeTest3.java
@@ -0,0 +1,37 @@
+package algonquin.cst2335.li000543;
+
+import static org.junit.Assert.assertNull;
+
+import android.content.Context;
+
+import androidx.room.Room;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import algonquin.cst2335.li000543.Recipe.RecipeDAO;
+import algonquin.cst2335.li000543.Recipe.RecipeDatabase;
+import algonquin.cst2335.li000543.Recipe.RecipeObject;
+
+@RunWith(AndroidJUnit4.class)
+public class RecipeTest3 {
+    @Test
+    public void testRecipeDeletion() {
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+
+        RecipeDatabase db = Room.inMemoryDatabaseBuilder(appContext, RecipeDatabase.class).allowMainThreadQueries().build();
+        RecipeDAO dao = db.recipeDAO();
+
+        RecipeObject recipeToDelete = new RecipeObject(2, "Delete Me", "Summary", "http://source.url", "http://image.url");
+        dao.insertRecipe(recipeToDelete);
+        dao.deleteRecipe(recipeToDelete);
+
+        RecipeObject result = dao.getRecipe(2);
+        assertNull(result);
+
+        db.close();
+    }
+
+}
