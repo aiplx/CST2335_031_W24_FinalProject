0a528b2a589b13f7b4079d496ef458b833263dee
itsolidude <flexibleoli.12@gmail.com>
2024-03-31 13:42:29 -0400

    Changed favorites layout - O.K.

--- app/src/main/java/algonquin/cst2335/lian0122/AppDatabase.java
+++ app/src/main/java/algonquin/cst2335/lian0122/AppDatabase.java
@@ -3,13 +3,12 @@ package algonquin.cst2335.lian0122;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {FavoriteLocation.class}, version = 1, exportSchema = false)
-
 /**
  * Represents the application's database.
  * Extends RoomDatabase to leverage Room's database capabilities.
  * Defines the database schema and version.
  */
+@Database(entities = {FavoriteLocation.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     /**
      * Provides access to LocationDao for database operations.
